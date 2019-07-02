6097a748e88bfaa80cf373fd0236ac02ff7bd034
ani <aniruddh.chugh@gmail.com>
2019-07-02 12:16:56 +0530

    added ObjectCreation

--- src/main/java/pojo/Product.java
+++ src/main/java/pojo/Product.java
@@ -2,4 +2,13 @@ package pojo;
 
 public class Product {
 	String displayLabel;
+
+	public String getDisplayLabel() {
+		return displayLabel;
+	}
+
+	public void setDisplayLabel(String displayLabel) {
+		this.displayLabel = displayLabel;
+	}
+	
 }
